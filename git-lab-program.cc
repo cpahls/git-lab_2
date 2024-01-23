7f65111261f599006ee6a3a62ef77e73d8caa897
cpahls <cp068622@ohio.edu>
2024-01-23 14:10:13 -0500

    final code

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Christian Pahls>
+ *        Date: <01/23/24>
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
